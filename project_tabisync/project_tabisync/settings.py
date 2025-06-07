44d0263929cc55552a0052dcc27343799b6f0be0
naga-T28 <172464414+naga-T28@users.noreply.github.com>
2025-06-08 00:46:24 +0900

    [update] : プライバシーポリシーと利用規約の追加

--- project_tabisync/project_tabisync/settings.py
+++ project_tabisync/project_tabisync/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,7 +29,7 @@ SECRET_KEY = '[REVOKED_DJANGO_SECRET_KEY]'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.0.193']
 
 
 # Application definition
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'project_tabisync.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")], #add_2025.06.07
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
