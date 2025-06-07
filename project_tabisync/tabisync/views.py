4e58a87d95c2ea0dce9f6c91106b7974b4dc8320
永田平拓幸 <hiroyuki.nagatahira@neuromagic.com>
2025-06-08 00:46:24 +0900

    [update] : プライバシーポリシーと利用規約の追加

--- project_tabisync/tabisync/views.py
+++ project_tabisync/tabisync/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView #add_2025.06.07
 
-# Create your views here.
+# ホーム画面を表示するビュー
+class HomeView(TemplateView):
+    template_name = "home.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context  
+# 利用規約
+class UserAgreementView(TemplateView):
+    template_name = "user_agreement.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+# プライバシーポリシー
+class PrivacyPolicyView(TemplateView):
+    template_name = "privacy_policy.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+    
+class CreateView(TemplateView):
+    template_name = "create.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+    
