5c543b00eabf7702fd0f84c9e9e82a2cee70d9be
永田平拓幸 <hiroyuki.nagatahira@neuromagic.com>
2025-06-11 10:36:29 +0900

    [update] ：しおり作成ページの追加

--- project_tabisync/project_tabisync/settings.py
+++ project_tabisync/project_tabisync/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-i@!cvz4i1di!o$yq#h3ga8ep_-2-z%&i((c-6lwj(c2v+jj_0r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.193']
+ALLOWED_HOSTS = ['192.168.0.193','0.0.0.0']
 
 
 # Application definition
--- project_tabisync/tabisync/admin.py
+++ project_tabisync/tabisync/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Itinerary, TravelDate, Schedule, Memo, Item
 
-# Register your models here.
+admin.site.register(Itinerary)
+admin.site.register(TravelDate)
+admin.site.register(Schedule)
+admin.site.register(Memo)
+admin.site.register(Item)
