70590927799a62bdaa88152af5a4e2a11ee040de
永田平拓幸 <hiroyuki.nagatahira@neuromagic.com>
2025-06-17 13:49:57 +0900

    entrypoint.shの修正

--- containers/django/entrypoint.sh
+++ containers/django/entrypoint.sh
@@ -4,6 +4,7 @@
 # Uncomment if needed
 # while ! nc -z db 5432; do sleep 1; done
 
+python3 manage.py collectstatic --noinput
 python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
 exec "$@"
