482b0577ac528e2a41e5c80bb2595b008acc795d
永田平拓幸 <hiroyuki.nagatahira@neuromagic.com>
2025-06-17 17:51:32 +0900

    [updates]

--- containers/django/entrypoint.sh
+++ containers/django/entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-
+cd /code/project_tabisync
 # DBが起動するまで待機（Postgresを使ってる場合など）
 # Uncomment if needed
 # while ! nc -z db 5432; do sleep 1; done
 
-python3 project_tabisync/manage.py collectstatic --noinput
-python3 project_tabisync/manage.py makemigrations --noinput
-python3 project_tabisync/manage.py migrate --noinput
+python3 manage.py collectstatic --noinput
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
 exec gunicorn project_tabisync.wsgi:application --bind 0.0.0.0:8000
 
 
