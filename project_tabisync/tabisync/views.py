4909d257580d4939671ae3217e0cd4f66975a4f2
naga-T28 <172464414+naga-T28@users.noreply.github.com>
2025-06-20 10:42:27 +0900

    [update] : v1.3.0 : 質問ページの追加

--- project_tabisync/tabisync/views.py
+++ project_tabisync/tabisync/views.py
@@ -82,6 +82,12 @@ class HomeView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         return context  
+# ホーム画面を表示するビュー
+class ProfileView(TemplateView):
+    template_name = "docs/profile.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context  
 # 利用規約
 class UserAgreementView(TemplateView):
     template_name = "docs/user_agreement.html"
@@ -89,6 +95,12 @@ class UserAgreementView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         return context
+# Q and Aを表示するビュー
+class QAView(TemplateView):
+    template_name = "docs/qanda.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context  
 # プライバシーポリシー
 class PrivacyPolicyView(TemplateView):
     template_name = "docs/privacy_policy.html"
